5706e4ce26efc5a3b6d853742da769f46dcb191d
Dr Maxim Orlovsky <dr.orlovsky@gmail.com>
2020-05-17 17:42:32 +0200

    AsAny trait

---
+++ src/common/as_any.rs
@@ -0,0 +1,20 @@
+// LNP/BP Core Library implementing LNPBP specifications & standards
+// Written in 2020 by
+//     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
+//  The convert.rs file written in 2020 by
+//     Martin Habovstiak <martin.habovstiak@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the MIT License
+// along with this software.
+// If not, see <https://opensource.org/licenses/MIT>.
+
+use std::any::Any;
+
+pub trait AsAny {
+    fn as_any(&self) -> &dyn Any;
+}
