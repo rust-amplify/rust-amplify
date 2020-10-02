5ba16113fa74d1659d8e6d8de7ed6146ccff815e
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-10-02 23:11:59 +0200

    Added a few trivial tests.

--- serde_str_helpers/src/lib.rs
+++ serde_str_helpers/src/lib.rs
@@ -118,4 +118,23 @@ mod tests {
 
         let _ = serde_json::from_str::<CheckDeser>("\"The Times 03/Jan/2009 Chancellor on brink of second bailout for banks\"").unwrap();
     }
+
+    #[test]
+    fn conversions() {
+        // I have no clue why Rust fails to infer the type.
+        let before_conversion = <Cow<'_, str>>::Borrowed("foo");
+        let after_conversion = Cow::from(DeserBorrowStr::from(before_conversion.clone()));
+        assert_eq!(after_conversion, before_conversion);
+
+        let before_conversion = <Cow::<'_, str>>::Owned("foo".to_string());
+        let after_conversion = Cow::from(DeserBorrowStr::from(before_conversion.clone()));
+        assert_eq!(after_conversion, before_conversion);
+    }
+
+    #[test]
+    fn deref() {
+        let string = "This isn't the kind of software where we can leave so many unresolved bugs that we need a tracker for them.";
+        let wrapped = DeserBorrowStr::from(<Cow<'_, str>>::Borrowed(string));
+        assert_eq!(&*wrapped, string);
+    }
 }
