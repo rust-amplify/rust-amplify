c56c243508a32c4710e494a0a6fc3c6d1456f183
Martin Habovstiak <martin.habovstiak@gmail.com>
2020-10-02 21:45:30 +0200

    Fixed a few nits

--- serde_str_helpers/src/lib.rs
+++ serde_str_helpers/src/lib.rs
@@ -3,7 +3,6 @@
 //
 // Written in 2019-2020 by
 //     Martin Habovstiak <martin.habovstiak@gmail.com>
-//     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
 //
 // To the extent possible under law, the author(s) have dedicated all
 // copyright and related and neighboring rights to this software to
