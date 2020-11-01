327fedc9f76295474fd8d57e6690d89f1fe562d0
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2020-11-01 01:16:31 +0100

    Auto From<T> for String impl for Error derive

--- derive/src/error.rs
+++ derive/src/error.rs
@@ -24,5 +24,11 @@ pub(crate) fn inner(input: DeriveInput) -> Result<TokenStream2> {
     Ok(quote! {
         impl #impl_generics ::std::error::Error for #ident_name #ty_generics #where_clause {
         }
+
+        impl #impl_generics From<#ident_name #ty_generics> for String #where_clause {
+            fn from(err: #ident_name #ty_generics) -> Self {
+                err.to_string()
+            }
+        }
     })
 }
