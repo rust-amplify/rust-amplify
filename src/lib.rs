fcaca7ed1223e477ad8a4943c0e015a7e8e3f78d
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-11-21 18:49:18 +0100

    Add re-exportable ascii dependency

--- src/lib.rs
+++ src/lib.rs
@@ -48,14 +48,18 @@ extern crate serde_crate as serde;
 extern crate amplify_num;
 #[cfg(any(test, feature = "hex"))]
 pub use num::hex;
+
 #[cfg(feature = "stringly_conversions")]
 pub extern crate stringly_conversions;
 #[cfg(feature = "stringly_conversions")]
 pub use stringly_conversions::*;
+
 #[cfg(feature = "proc_attr")]
 pub extern crate amplify_syn as proc_attr;
 #[cfg(feature = "proc_attr")]
 pub use proc_attr::ident;
+
+pub extern crate ascii;
 
 #[macro_use]
 mod macros;
